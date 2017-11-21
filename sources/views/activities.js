f17a9029d013040fa23ef9c036802f705a6b2371
P0s7el2 <romeo.com.ru@mail.ru>
2017-11-21 23:01:37 +0300

    Update activities.js

--- sources/views/activities.js
+++ sources/views/activities.js
@@ -17,12 +17,9 @@ var grid =
 	columns:[
 		{id:"id", header:" ", width:35, template:"<span  class='webix_icon_btn fa-square-o'></span>"},
 		
-		{id:"code", header:["Code", {content:"textFilter"} ], sort:"string", minWidth:80, fillspace: 1},
+		{id:"Details", header:["Details", {content:"selectFilter"} ], sort:"string", minWidth:120, fillspace: 2, editor:"text",  template:"<div class='category#activitytypes#'>#activitytypes#</div>"},
+		{id:"DueDate", header:["Due date", {content:"textFilter"} ], sort:"string", minWidth:80, fillspace: 1},
 		{id:"name", header:["Name", {content:"textFilter"} ], sort:"string", minWidth:120, fillspace: 2, editor:"text"},
-		{id:"categoryName", header:["Category", {content:"selectFilter"} ], sort:"string", minWidth:120, fillspace: 2, editor:"text",  template:"<div class='category#category#'>#categoryName#</div>"},
-		{id:"price", header:["Price"], sort:"int", minWidth:80, fillspace: 1, format:webix.i18n.priceFormat},
-		{id:"quantity", header:["Quantity" ], sort:"int", minWidth:60, fillspace: 1},
-		{id:"statusName", header:["Status"], minWidth:75, sort:"string", fillspace: 1, template:"<span class='status status#status#'>#statusName#</span>"},
 
 		{id:"edit", header:"&nbsp;", width:35, template:"<span  style=' cursor:pointer;' class='webix_icon fa-pencil'></span>"},
 		{id:"delete", header:"&nbsp;", width:35, template:"<span  style='cursor:pointer;' class='webix_icon fa-trash-o'></span>"}
